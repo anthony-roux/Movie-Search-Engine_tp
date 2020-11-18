27f9afce8e1125d63ef70519ba6ee6e48a15faf2
anthony-roux <roux.anthony@hetic.net>
2020-11-18 18:05:08 +0100

    Create Header component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
 //import logo from './logo.svg';
 import './App.css';
+import '../../scss/style.scss';
+
 import useFilms from '../../hooks/useFilms/useFilms.js'
 import useGenre from '../../hooks/useGenre/useGenre.js'
 import useFilmsId from '../../hooks/useFilmsId/useFilmsId.js'
 import HomePage from '../HomePage/HomePage.js'
 import MovieDetail from '../MovieDetail/MovieDetail.js'
 import {  BrowserRouter, Route } from 'react-router-dom'
+import Header from '../Header/Header';
 
 function App() {
   const film =useFilms();
@@ -16,6 +19,7 @@ function App() {
   console.log(filmid);
   return (
     <div className="App">
+      <Header />
       <BrowserRouter>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/MovieDetail" component={MovieDetail}></Route>
