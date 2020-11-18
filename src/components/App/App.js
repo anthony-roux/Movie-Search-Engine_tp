e920ad7ffe73d0fe72521352684160fcd5416d15
Renaud <renaudfradin@gmail.com>
2020-11-18 15:35:43 +0100

    route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,19 @@
 //import logo from './logo.svg';
 import './App.css';
-//import useFilms from '../src/hooks/useFilms/useFilms.js'
-//import useGenre from '../src/hooks/useGenre/useGenre.js'
-//import useFilmsId from '../src/hooks/useFilmsId/useFilmsId.js'
+import useFilms from '../../hooks/useFilms/useFilms.js'
+import useGenre from '../../hooks/useGenre/useGenre.js'
+import useFilmsId from '../../hooks/useFilmsId/useFilmsId.js'
 import HomePage from '../HomePage/HomePage.js'
 import MovieDetail from '../MovieDetail/MovieDetail.js'
 import {  BrowserRouter, Route } from 'react-router-dom'
 
-function App() {/*
+function App() {
   const film =useFilms();
   const genre =useGenre();
   const filmid =useFilmsId();
   console.log(genre);
   console.log(film);
-  console.log(filmid);*/
+  console.log(filmid);
   return (
     <div className="App">
       <BrowserRouter>
