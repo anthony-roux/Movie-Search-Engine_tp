0b22b38219d715a3ff176612406f6fbc0698a420
anthony-roux <roux.anthony@hetic.net>
2020-11-19 12:20:38 +0100

    create responsive footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,24 +11,28 @@ function Footer() {
 
   return (
     <footer className="footer">
-      <img className="footer__logo" src={logo} alt="logo"/>
+      <div class="footer__logoContainer">
+        <img className="footer__logo" src={logo} alt="logo"/>
+      </div>
       <div class="footer__author">
         <p>REACT PROJECT <br></br><span>designed and developed by</span></p>
-        <a href="#" class="author">Wendy <span>Paulos</span></a>
-        <a href="#" class="author">Jacky <span>Truong</span></a>
-        <a href="#" class="author">Renaud <span>Fradin</span></a>
-        <a href="#" class="author">Emmy <span>Peteytas</span></a>
-        <a href="#" class="author">Annie <span>Tran</span></a>
-        <a href="#" class="author">Priska <span>Sumampow</span></a>
-        <a href="#" class="author">Dorian <span>Vidal</span></a>
-        <a href="#" class="author">Anthony <span>Roux</span></a>
-        <a href="#" class="author">Oum <span>Mauduit</span></a>
-        <a href="#" class="author">Yoan <span>Bensimhon</span></a>
-        <a href="#" class="author">Tong <span>Xing</span></a>
+        <div class="author__container">
+          <a href="https://github.com/WPaulos" target="_blank" rel="noreferrer" class="author">Wendy <span>Paulos</span></a>
+          <a href="https://github.com/jackytruonghetic" target="_blank" rel="noreferrer" class="author">Jacky <span>Truong</span></a>
+          <a href="https://github.com/Renaudfradin" target="_blank" rel="noreferrer" class="author">Renaud <span>Fradin</span></a>
+          <a href="https://github.com/Amy-Dev" target="_blank" rel="noreferrer" class="author">Emmy <span>Peteytas</span></a>
+          <a href="https://github.com/annietth" target="_blank" rel="noreferrer" class="author">Annie <span>Tran</span></a>
+          <a href="https://github.com/SP-Sumampow" target="_blank" rel="noreferrer" class="author">Priska <span>Sumampow</span></a>
+          <a href="https://github.com/dorian-vidal" target="_blank" rel="noreferrer" class="author">Dorian <span>Vidal</span></a>
+          <a href="https://github.com/anthony-roux" target="_blank" rel="noreferrer" class="author">Anthony <span>Roux</span></a>
+          <a href="https://github.com/Mauduit-O" target="_blank" rel="noreferrer" class="author">Oum <span>Mauduit</span></a>
+          <a href="https://github.com/yo-gif12" target="_blank" rel="noreferrer" class="author">Yoan <span>Bensimhon</span></a>
+          <a href="https://github.com/tiphanieTong" target="_blank" rel="noreferrer" class="author">Tong <span>Xing</span></a>
+        </div>
       </div>
       <div class="footer__legalMentions">
         <p>Copyright ©2020</p>
-        <p>Données venant de l’API IMDB</p>
+        <a href="https://developer.imdb.com/" target="_blank" rel="noreferrer">Données venant de l’API IMDB</a>
       </div>
     </footer>
 
