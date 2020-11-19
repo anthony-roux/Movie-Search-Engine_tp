270204f3f2efb10c89280399ccd662ce801057a0
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-11-19 15:47:43 +0100

    add carousel to the popularMovies

--- src/components/CardMovie/CardPopularMovies.js
+++ src/components/CardMovie/CardPopularMovies.js
@@ -1,6 +1,9 @@
-import React, { useEffect, useState} from 'react';
-import { getfilms } from '../../service/films/films.js'
+import React from 'react';
+//import { getfilms } from '../../service/films/films.js'
 import CardMovie from './CardMovie.js';
+
+import Carousel from 'react-elastic-carousel';
+import Slider from '../Slider/Slider.js'
 
 const baseUrl = "http://image.tmdb.org/t/p/w185/";
 
@@ -8,9 +11,17 @@ const baseUrl = "http://image.tmdb.org/t/p/w185/";
   function CardPopularMovies(props) {
 
     let genresList = props.genres.genres
+
+    const breakPoint = [
+      {width: 320, itemsToShow: 1},
+      {width: 500, itemsToShow: 2},
+      {width: 700, itemsToShow: 3 },
+      {width: 1000, itemsToShow: 4},
+      {width: 1200, itemsToShow: 5},
+    ]
  
     let listOfCardPopularMovie = props.movies.map(movie => { 
-      console.log(`lil ${genresList}`)
+      
       let genres = []
       if (typeof genresList !== "undefined") {
         genres = movie.genre_ids.map(genreId => {
@@ -21,14 +32,18 @@ const baseUrl = "http://image.tmdb.org/t/p/w185/";
       console.log(`lil ${genres}`)
 
       return (
-        <CardMovie imgsrc={baseUrl + movie.poster_path} title={movie.title} genres={genres} />
+        <Slider cardMovie={<CardMovie imgsrc={baseUrl + movie.poster_path} title={movie.title} genres={genres} />}/>
       )
     })
 
     console.log(listOfCardPopularMovie)
     return (
       <div>
-        {listOfCardPopularMovie}
+
+        <Carousel breakPoints={breakPoint}>
+          {listOfCardPopularMovie}
+        </Carousel>
+        {/* {listOfCardPopularMovie} */}
       </div>
     )
   }
