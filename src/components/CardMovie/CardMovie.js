14cd1b6657c1931a2f0ec35a6ddcc5f6b66f3882
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-11-19 12:47:57 +0100

    card movie component

--- src/components/CardMovie/CardMovie.js
+++ src/components/CardMovie/CardMovie.js
@@ -1,13 +1,21 @@
+import React from "react";
+import "./CardMovie.scss";
 
 function CardMovie(props) {
-  
+    let genresString = props.genres.map((genre) => {
+        if (typeof genre === "undefined") {
+            return "";
+        }
+        return genre.name
+    }).join("/");
+    // let genresString = "toto";
 return (
 <div>
     <div className="card">
-        <img src={props.imgsrc} className="card-img" alt=""/>
-            <div className="card-body">
-                <h1>{propsTitle}</h1>
-                <p className="card-text gern">{props.gern}</p>
+        <img src={props.imgsrc} className="card_img" alt=""/>
+            <div className="card_body">
+                <h1 className="card_title">{props.title}</h1>
+                <p className="card_gern">{genresString}</p>
             </div>
     </div>
 </div>
