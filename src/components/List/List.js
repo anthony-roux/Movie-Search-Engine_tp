3f8510e3879c41e16faef083af056ee207b14c5e
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-11-20 12:23:49 +0100

    delete unused folders

--- src/components/List/List.js
+++
@@ -1,9 +0,0 @@
-function List(props) {
-    const { films } = props;
-    return (
-        <div>
-            {films && films.map(fil => <div key={fil.id}></div> )}
-        </div>
-    );
-}
-export default List;
