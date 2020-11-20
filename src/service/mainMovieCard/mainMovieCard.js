33e41e4b26eb3d4d6bca6ec0e7fdcae113d1e625
Sentiany Priska Sumampow <vivian.priska@gmail.com>
2020-11-20 12:32:45 +0100

    clean code

--- src/service/mainMovieCard/mainMovieCard.js
+++ src/service/mainMovieCard/mainMovieCard.js
@@ -3,17 +3,3 @@ export function getmainfilms() {
   return fetch('https://api.themoviedb.org/3/search/movie?api_key=402fb26d39f4da70880cbc8db2dcc9b7&language=fr-FR&query='+name)
   .then(reponse => reponse.json())
 }
-
-
-// export function getimage(id) {
-//   const name = "bob l'éponge,"
-//   return fetch('https://api.themoviedb.org/3/movie/'{id}'/images?api_key=<<api_key>>&language=en-US')
-//   .then(reponse => reponse.json())
-// }
-
-
-// export function getTitle() {
-//   const name = "bob l'éponge,"
-//   return fetch('https://api.themoviedb.org/3/search/movie?api_key=402fb26d39f4da70880cbc8db2dcc9b7&language=fr-FR&query='+name)
-//   .then(reponse => reponse.json())
-// }
